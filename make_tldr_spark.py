ba5c832cf2a58e8b92332612d67af993db35349c
bryparker <brydon.parker@mail.utoronto.ca>
2019-06-25 16:02:07 -0400

    forgot to add spark tldr maker

--- make_tldr_spark.py
+++ make_tldr_spark.py
@@ -8,6 +8,7 @@ from pyspark.sql import Row,SparkSession,HiveContext
 from pyspark.sql.functions import col,size,explode,split
 from pyspark.sql.types import StringType,IntegerType,ArrayType
 from pyspark.sql.functions import udf, array, length
+from data.tldr.make_tldr_helpers import tl_dr, get_all_tldr, iter_tldr, botlist
 
 spark = SparkSession.builder.appName("webis-tldr-17-corpus-construction").getOrCreate()
 sc = spark.sparkContext
@@ -55,48 +56,6 @@ Sources:
 [2] https://www.reddit.com/r/autowikibot/wiki/redditbots
 '''
 
-global botlist
-with open('./resources/botlist.csv','r') as bot_file:
-    reader=csv.reader(bot_file)
-    botlist=list(reader)
-botlist = list(itertools.chain.from_iterable(botlist))
-
-# Check for presence of a tldr pattern
-def tl_dr(input):
-    lower_text = str(input).lower()
-    match = re.search(r'tl.{0,3}dr',str(lower_text))
-    if match:
-        return input
-    else:
-        return None
-
-# Find all the matched tldr patterns
-def get_all_tldr(input):
-    lower = str(input).lower()
-    pattern=re.compile(r'tl.{0,3}dr') 
-    return pattern.findall(lower)
-
-# Find location of the tldr pattern and split text to form <content, summary> pairs
-def iter_tldr(text):
-    lower_text = str(text).lower()
-    patterns = re.findall(r'tl.{0,3}dr',lower_text)
-    if len(patterns) > 0:
-        match = patterns[-1]
-        if match:
-            index = lower_text.rfind(match)
-            if index == 0 or index+len(match) == len(str(lower_text)):
-                return None
-            else:
-                content = text[:index].strip()
-                summary = text[index+len(match):].strip()
-                if len(content.split()) > len(summary.split()):
-                    return [content,summary]
-                else:
-                    return None
-        else:
-            return None
-    else:
-        return None
 
 # Define all the UDFs to be applied on the dataframes
 removeBotPosts = udf(lambda input:None if str(input) in botlist else input, StringType())
